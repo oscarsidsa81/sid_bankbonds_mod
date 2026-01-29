6fecdca1849abbba838e88197035399423c2cb28
oscarsidsa81 <ovilches@sid-sa.com>
2026-01-29 08:23:40 +0100

    initial commit

--- __init__.py
+++ __init__.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from . import models
-from . import hooks
+
+# Odoo expects the hook function to be importable from the module top-level
+# (odoo.addons.<module_name>), so we re-export it here.
+from .hooks import post_init_migrate_from_studio  # noqa: F401
